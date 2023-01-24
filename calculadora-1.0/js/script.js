64f6f39859ff1afc512d3b0d0d04d3ca83794b4b
Ely Carvalho <elycarvalho3@gmail.com>
2023-01-24 04:12:09 -0300

    Add files via upload

---
+++ calculadora-1.0/js/script.js
@@ -0,0 +1,104 @@
+let visor = document.querySelector(".visor")
+let num1 = 0
+let num2 = 0
+let operador = ''
+let operacao = ''
+let digitos = ''
+let resultado = ''
+
+function processaTecla(numero) {
+  console.log(numero)
+  digitos += numero
+  visor.innerHTML = digitos
+  console.log(digitos)
+}
+
+function adicao() {
+	num1 = digitos
+	digitos = ''
+	operador = '+'
+	visor.innerHTML += '+'
+}
+
+function subtracao() {
+	num1 = digitos
+	digitos = ''
+	operador = '-'
+	visor.innerHTML += '-'
+}
+
+function multiplica() {
+	num1 = digitos
+	digitos = ''
+	operador = 'x'
+	visor.innerHTML += 'X'
+}
+
+function divisao() {
+	num1 = digitos
+	digitos = ''
+	operador = '/'
+	visor.innerHTML += '/'
+}
+
+function porcentagem() {
+	num1 = digitos
+	digitos = ''
+	operador = '%'
+	visor.innerHTML += '%'
+}
+
+function elevado2() {
+	num1 = digitos
+	digitos = ''
+	resultado = parseFloat(num1) * parseFloat(num1)
+	visor.innerHTML = resultado
+	digitos = resultado
+}
+
+function raizQuadrada() {
+	num1 = digitos
+	digitos = ''
+	resultado = Math.sqrt(parseFloat(num1))
+	visor.innerHTML = resultado
+	digitos = resultado
+}
+
+function calcular() {
+	num2 = digitos
+	if(operador === '+') {
+		resultado = parseFloat(num1) + parseFloat(num2)
+		visor.innerHTML = resultado
+		digitos = resultado
+	} else if(operador === '-') {
+		  resultado = parseFloat(num1) - parseFloat(num2)
+		  visor.innerHTML = resultado
+		  digitos = resultado
+	} else if(operador === 'x') {
+		  resultado = parseFloat(num1) * parseFloat(num2)
+		  visor.innerHTML = resultado
+		  digitos = resultado
+	} else if(operador === '/') {
+		  resultado = parseFloat(num1) / parseFloat(num2)
+		  visor.innerHTML = resultado
+		  digitos = resultado
+	} else if(operador === '%') {
+		  resultado = (parseFloat(num1) * parseFloat(num2)) / 100
+		  visor.innerHTML = resultado
+		  digitos = resultado
+	}
+}
+
+function limpa() {
+	num1 = ''
+	num2 = ''
+	resultado = ''
+	operador = ''
+	digitos = ''
+	visor.innerHTML = ''
+}
+
+function apaga() {
+	digitos = ''
+	visor.innerHTML = digitos
+}
