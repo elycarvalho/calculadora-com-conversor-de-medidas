c61b3a4a367333b29be247ec60f5a177c88a1a5f
Ely Carvalho <elycarvalho3@gmail.com>
2023-02-22 03:43:23 -0300

    Add files via upload

---
+++ calculadora-2.0/js/conversoes.js
@@ -0,0 +1,29 @@
+const menuConv = document.querySelector('.menu-conversoes')
+
+function pesMetros() {
+	
+}
+
+function milhasKilometros() {
+
+}
+
+function polegadasCm() {
+
+}
+
+function oncaKilos() {
+
+}
+
+function fahrenheitCelsius() {
+
+}
+
+function menuConversoes() {
+	menuConv.style.display = 'flex'
+}
+
+function fechaMenuConv() {
+	menuConv.style.display = 'none'
+}
